db6dd1f9a2b55dd8ae600a5196d830dcd310e413
teomankinaci <teoman.kinaci@iubh-dualesstudium.de>
2021-07-28 02:19:04 +0200

    add Task endpoints

--- backend/src/controller/task.controller.ts
+++ backend/src/controller/task.controller.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
-import { Task } from "../entity/task.model";
+import { Task, taskSchema } from "../entity/task.model";
+import { getRepository } from "typeorm";
 
 type GetAllTasksResponseBody = {
   tasks: Task[];
@@ -13,33 +14,89 @@ type CreateTaskResponseBody = {
   createdTask: Task;
 };
 
-type CreateTaskRequestBody = Omit<Task, "id">;
+type CreateTaskRequestBody = {
+  task: Omit<Task, "id">;
+};
 
 type UpdateTaskResponseBody = {
-  createdTask: Task;
+  updatedTask: Task;
 };
 
-type UpdateTaskRequestBody = Partial<Omit<Task, "id">>;
-
-export const getAllTasks: RequestHandler<{}, GetAllTasksResponseBody> = (
-  req,
-  res
-) => {
-  console.log("Get All Tasks");
-  res.send();
+type UpdateTaskRequestBody = {
+  updateTask: Partial<Omit<Task, "id">>;
 };
-export const getTask: RequestHandler<{ id: string }, GetTaskResponseBody> = (
-  req,
-  res
-) => {};
+
+export const getAllTasks: RequestHandler<void, GetAllTasksResponseBody> =
+  async (req, res) => {
+    const repository = await getRepository(Task);
+    const tasks = repository.find({});
+    console.log("GRÖßE VOM REPO: " + repository.find({ relations: ["task"] }));
+    console.log(tasks);
+    console.log("called getAllTasks");
+    //res.send(tasks);
+  };
+export const getTask: RequestHandler<{ id: string }, GetTaskResponseBody> =
+  async (req, res) => {
+    console.log("GET TASK CALLED id: " + req.params.id);
+    const taskRepository = await getRepository(Task);
+    const task = await taskRepository.findOne(req.params.id);
+    if (task !== undefined){
+      res.send({ task: task });
+    } else {
+      console.log("NO TASK FOUND id: " + req.params.id);
+      res.sendStatus(400);
+    }
+  };
 export const updateTask: RequestHandler<
   { id: string },
   UpdateTaskResponseBody,
   UpdateTaskRequestBody
-> = (req, res) => {};
+> = async (req, res) => {
+  const repository = await getRepository(Task);
+  await repository.update(req.params.id, req.body.updateTask);
+  const updatedTask = await repository.findOneOrFail(req.params.id);
+  res.send({ updatedTask: updatedTask });
+};
+
 export const createTask: RequestHandler<
-  {},
-  CreateTaskResponseBody,
-  CreateTaskRequestBody
-> = (req, res) => {};
-export const deleteTask: RequestHandler<{ id: string }, {}> = (req, res) => {};
+  CreateTaskRequestBody,
+  CreateTaskResponseBody
+> = async (req, res) => {
+  const taskRepository = await getRepository(Task);
+  console.log("called createTask");
+  console.log("req.body = " + req.body.task);
+
+  try {
+    const validateBody = await taskSchema.validate(req.body.task);
+    const validTask = Task.create(validateBody);
+    //const task = Task.create(req.body.task);
+    const countHasTaskWithName = await taskRepository.count({
+      name: validTask.name,
+    });
+    if (countHasTaskWithName > 0) {
+      console.log("ICH HABE RICHTIG GEZÄHLT");
+      throw new Error();
+    }
+    const savedTask = await taskRepository.save(validTask);
+    console.log("SAVED new Task (in createTask)");
+    res.send({ createdTask: savedTask });
+  } catch (e) {
+    console.log("NAME EXISTIERT BEREITS" + e.message);
+    res.sendStatus(400);
+  }
+};
+
+export const deleteTask: RequestHandler<{ id: string }, string> = async (
+  req,
+  res
+) => {
+  const repository = await getRepository(Task);
+  const foundTask = repository.findOneOrFail(req.params.id);
+  if (foundTask.catch()) {
+    console.error("KEINE ID ZUM LÖSCHEN GEFUNDEN");
+    res.send("There is no Task to delete with the id: " + req.params.id);
+  }
+  console.log("ID GEFUNDEN");
+  repository.delete(req.params.id);
+  res.sendStatus(res.statusCode);
+};
--- backend/src/entity/task.model.ts
+++ backend/src/entity/task.model.ts
@@ -1,10 +1,18 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 import * as yup from "yup";
+
+interface TaskInitProps {
+  name: string;
+  description: string;
+  //createdAt: Date;
+  //updatedAt: Date;
+}
 
 export const taskSchema = yup.object().shape({
   name: yup.string().required(),
   description: yup.string().required(),
-  //....
+  //createdAt: yup.date().required(),
+  //updatedAt: yup.date().required(),
 });
 
 @Entity()
@@ -17,5 +25,27 @@ export class Task {
 
   @Column()
   description!: string;
-  //....
+
+  @CreateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    onUpdate: "CURRENT_TIMESTAMP(6)",
+  })
+  updatedAt!: Date;
+
+  static create(props: TaskInitProps): Task {
+    const task = new Task();
+    const current_timestamp = new Date();
+    task.name = props.name;
+    task.description = props.description;
+    task.createdAt = current_timestamp;
+    task.updatedAt = current_timestamp;
+    return task;
+  }
 }
