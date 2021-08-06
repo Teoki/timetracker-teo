f1a4032b8b86bcd1816545cb90fe046e9457a586
teomankinaci <teoman.kinaci@iubh-dualesstudium.de>
2021-08-06 16:45:31 +0200

    add material-ui and material-table to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,43 @@
 import * as React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import MaterialTable from 'material-table';
+import {useState} from "react";
 
 function App() {
-  React.useEffect(() => {
-    fetch("/api");
+
+    const [data, setData] = useState([])
+    const columns = [
+        {title: "ID", field: "id"},
+        {title: "Name", field: "name"},
+        {title: "Description", field: "description"},
+        {title: "Created at", field: "createdAt"},
+        {title: "Updated at", field: "updatedAt"}
+    ]
+
+    React.useEffect(() => {
+    fetch("/api/task/")
+        .then(resp=>resp.json())
+        .then(resp=>{
+            console.log(resp)
+            setData(resp)})
   }, []);
+
+  /*function openCreateTaskInputField() {
+    return <div>Hi</div>;
+  }*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MaterialTable
+          title="All Tasks"
+          data={data}
+          columns={columns}
+      />
+      <Button /*onClick={openCreateTaskInputField}*/ variant="contained" color="primary">
+        Create Task
+      </Button>
+
     </div>
   );
 }
